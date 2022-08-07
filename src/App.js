c9a9fea294f740f86c0d2f4529936881f1b287c1
Yasha <yassine.damiri@epita.fr>
2022-08-07 01:05:24 +0000

    struct: Added presentation and Maintitle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Suspense, useEffect, useRef } from 'react';
+import Wave from 'react-wavify'
+
 
 /* Components */
 import Scene from './components/Scene';
 import NavBar from './components/Navbar';
 import MainTitle from './components/MainTitle';
+import Presentation from './components/Presentation';
+import MyWork from './components/MyWork';
 
 
 export default function App() {
   return (
     <div className='flex mx-5 my-5 w-screen h-screen bg-green-500' name='appBody'>
       <div className='flex flex-col flex-auto bg-green-500'>
-        <NavBar/>
-        <MainTitle/>
+        <NavBar />
+        <MainTitle />
+        <Presentation />
+        <MyWork />
       </div>
     </div>
   );
---
+++ src/components/MyWork.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+export default function MyWork(){
+
+    return (
+        <section className="flex-none mt-10">
+            <div className="mx-10">
+                <div className="bg-red-500">
+                    My Work
+                </div>
+            </div>
+        </section>
+    );
+
+}
