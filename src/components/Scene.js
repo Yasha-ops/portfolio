a242365eba7c09f6fc88014c7096aa235c1f93df
Yasha <yassinedamiri007@gmail.com>
2022-08-27 06:14:40 +0200

    main_page: Added text and code refractored

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,56 +1,22 @@
 import React, { Suspense, useEffect, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas,useFrame} from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Model } from './Avatar'
 
 import * as dat from 'dat.gui';
 
-export default function Scene() {
-
+function Head(){
   const mesh = useRef(null);
+  return (
+  <mesh ref={mesh} scale={5} position={[1.17, -2.48, 0]}>
+        <Suspense fallback={null}>
+          <Model position={[-0.12, -0.1, 0.7]} /> /* highlight-line */
+        </Suspense>
+    </mesh>
+  );
+}
 
-  useEffect(() => {
-      const gui = new dat.GUI({width:400});
-
-      const debugObject = {
-        position: {
-          x : 0,
-          y : 0,
-          z : 0
-        }
-      }
-
-      gui.add(debugObject.position, 'x')
-          .min(-10)
-          .max(10)
-          .step(0.01)
-          .onChange( () => {
-              mesh.current.position.x = debugObject.position.x;
-          })
-          .name("Pos x");
-      
-      
-      gui.add(debugObject.position, 'y')
-          .min(-10)
-          .max(10)
-          .step(0.01)
-          .onChange( () => {
-              mesh.current.position.y = debugObject.position.y;
-          })
-          .name("Pos y");
-          
-      gui.add(debugObject.position, 'z')
-          .min(-10)
-          .max(10)
-          .step(0.01)
-          .onChange( () => {
-              mesh.current.position.z = debugObject.position.z;
-          })
-          .name("Pos z");
-
-      return () => gui.destroy();
-  }, []);
-
+export default function Scene() {
   return (
     <Canvas
       camera={{ position: [2, 0, 12.25], fov: 15 }}
@@ -61,11 +27,7 @@ export default function Scene() {
     >
       <ambientLight intensity={1.25} />
       <directionalLight intensity={0.4} />
-      <mesh ref={mesh} scale={5} position={[1.17, -2.48, 0]}>
-        <Suspense fallback={null}>
-          <Model position={[-0.12, -0.1, 0.7]} /> /* highlight-line */
-        </Suspense>
-      </mesh>
+      <Head/>
     </Canvas>
   );
 }
