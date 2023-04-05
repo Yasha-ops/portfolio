b318ae5932d7d38f618a583a460eff9f458ee223
Yasha <yassinedamiri007@gmail.com>
2023-04-05 22:01:28 +0200

    fix(about-me): Fix reposnsive for Md

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,17 +4,20 @@ import "~/styles/Skills.css"
 
 
 class AboutMe extends React.Component {
-    render () {
+    render() {
         return (
-            <section className="min-h-screen-custom w-full bg-primary text-left h-auto" id='about-me'>
-                <h1 className="px-4 text-white font-bold text-4xl pt-20 tracking-wider">About me</h1>
-                <p className="p-4 pt-9 text-white text-lg font-bold pb-[100px]">
-                    I'm a student with a passion for securing digital systems and networks.
-                    My areas of expertise include cybersecurity, system administration,
-                    and network engineering. I've gained experience implementing security
+            <section className="w-full bg-primary text-left h-auto md:flex md:flex-col justify-center items-center pb-10 md:text-center" id='about-me'>
+                <div className="w-full h-full md:w-1/2">
+                    <h1 className="px-4 text-white font-bold text-4xl pt-20 tracking-wider">About me</h1>
+                    <p className="p-4 pt-9 text-white text-lg font-bold pb-[100px]">
+                        I'm a student with a passion for securing digital systems and networks.
+                        My areas of expertise include cybersecurity, system administration,
+                        and network engineering. I've gained experience implementing security
                         measures like firewalls and antivirus software, as well as performing
-                    system and network assessments to identify and address vulnerabilities.
-                </p>
+                        system and network assessments to identify and address vulnerabilities.
+                    </p>
+
+                </div>
             </section>
         );
     }
