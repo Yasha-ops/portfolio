982fe340373e83b57a1293914dffbe90187a46ae
Yassine Damiri <yassinedamiri007@gmail.com>
2024-12-23 20:55:13 +0100

    Fix responsiveness Projects

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,8 +14,8 @@ const Home: React.FC<{}> = () => {
       <HomeHero />
       <HomeAbout />
       <HomeExperience />
-      {/*<HomeProjects />
-      <HomeHackathons />
+      <HomeProjects />
+      {/*<HomeHackathons />
       <HomeGetInTouch />*/}
     </>
   );
